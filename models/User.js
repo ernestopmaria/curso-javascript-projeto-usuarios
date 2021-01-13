5466daa2145c0a01ae5febc42d2a8cc1d3e8f287
Ernesto Maria <58423237+ernestopmaria@users.noreply.github.com>
2021-01-13 21:07:34 +0000

    OOP MVC

---
+++ models/User.js
@@ -0,0 +1,13 @@
+class User {
+    constructor(name, gender, birth, country, email, password , photo, admin){
+        this.name=name,
+        this.name=gender,
+        this.name=birth,
+        this.name=country,
+        this.name=email,
+        this.name=password,
+        this.name=photo,
+        this.name=admin,
+
+    }
+}
