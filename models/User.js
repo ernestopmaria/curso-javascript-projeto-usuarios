b881246af0c4a808e4acc7e9b2e117aa1bb1b085
Ernesto Maria <58423237+ernestopmaria@users.noreply.github.com>
2021-01-14 16:46:16 +0100

    up

--- models/User.js
+++ models/User.js
@@ -1,13 +1,13 @@
 class User {
     constructor(name, gender, birth, country, email, password , photo, admin){
         this.name=name,
-        this.name=gender,
-        this.name=birth,
-        this.name=country,
-        this.name=email,
-        this.name=password,
-        this.name=photo,
-        this.name=admin,
+        this.gender=gender,
+        this.birth=birth,
+        this.country=country,
+        this.email=email,
+        this.password=password,
+        this.photo=photo,
+        this.admin=admin
 
     }
 }
