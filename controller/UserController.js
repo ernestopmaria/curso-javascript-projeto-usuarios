6cbcd2c2f8e9091966054fe16318d98189912dc6
Ernesto Maria <58423237+ernestopmaria@users.noreply.github.com>
2021-01-18 16:22:13 +0100

    operador ternario

--- controller/UserController.js
+++ controller/UserController.js
@@ -10,12 +10,54 @@ class UserController{
         
         this.formEl.addEventListener("submit", event=>{
             event.preventDefault();
-            this.addLine(this.getValues());
 
+            let values = this.getValues();
+          this.getPhoto().then(
+              (content)=>{
+                values.photo= content;
+                this.addLine(values);
+          }, (e)=>{
+              console.error(e)
+
+          })          
             
          
         });
     }
+
+    getPhoto(){
+
+        return new Promise ((resolve, reject)=>{
+            
+        let fileReader = new FileReader();
+
+        let elements =[...this.formEl.elements].filter(item=>{
+            if (item.name ==="photo"){
+                return item
+            } 
+        });
+       let file = (elements[0].files[0]);
+
+        fileReader.onload =()=>{
+
+            resolve(fileReader.result)
+
+        }
+        fileReader.onerror =(e)=>{
+            reject(e)
+
+        }
+         if(file){
+             fileReader.readAsDataURL(file)
+            }
+            else {
+                resolve("dist/img/boxed-bg.jpg");
+            }
+        });
+
+    }
+
+
     getValues(){
 
             let user = {};
@@ -26,12 +68,17 @@ class UserController{
                     if(field.checked){  
                         user[field.name] = field.value;
                 }
-                }else{
+                }else if(field.name =="admin"){
+                    user[field.name] = field.checked;
+                }
+                
+                
+                else{
                     user[field.name] = field.value;
                 }
             
             });
-            console.log(user)
+           
             return new User(
                 user.name,
                 user.gender, 
@@ -46,22 +93,23 @@ class UserController{
        
             }
     addLine(dataUser){
-    
-       
-                this.tableEl.innerHTML =` 
-                <tr>
+    let tr = document.createElement('tr');
+
+
+            tr.innerHTML =` 
                 <td>
-                  <img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm">
+                  <img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm">
                 </td>
                 <td>${dataUser.name}</td>
                 <td>${dataUser.email}</td>
-                <td>${dataUser.admin}</td>
+                <td>${(dataUser.admin)?'Sim':'Não'}</td>
                 <td>${dataUser.birth}</td>
                 <td>
                   <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
                   <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
                 </td>
-              </tr>`;
+             `;
+             this.tableEl.appendChild(tr)
              
               
             }
