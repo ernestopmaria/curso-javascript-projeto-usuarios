d8d9c29ba27c487d48be0684e607b02d419812f2
Ernesto Maria <58423237+ernestopmaria@users.noreply.github.com>
2021-01-19 20:08:42 +0000

    Update UserController.js

--- controller/UserController.js
+++ controller/UserController.js
@@ -11,11 +11,19 @@ class UserController{
         this.formEl.addEventListener("submit", event=>{
             event.preventDefault();
 
+            let btn = this.formEl.querySelector("[type=submit]");
+            
+            btn.disabled = true;
+
             let values = this.getValues();
           this.getPhoto().then(
               (content)=>{
                 values.photo= content;
                 this.addLine(values);
+
+                this.formEl.reset();
+
+                btn.disabled=false
           }, (e)=>{
               console.error(e)
 
